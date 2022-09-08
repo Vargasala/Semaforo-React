b43a1d1719fe5e738605eb3de336f309c18dec1c
Vargasala <vargasala@gmail.com>
2022-09-08 19:51:52 +0000

    Semaforo final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,45 +7,63 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	// valor inicial, la funcion que actualiza el valor inicial
 	//     "hola"
-	const [state,setState]= useState("Hola"); //2. usar el hooks useState
-	const [color,setColor]= useState("btn-success");
+	const [colorAmarillo,setColorAmarillo]= useState("bg-opacity-25"); //2. usar el hooks useState
+	const [colorVerde,setColorVerde]= useState("bg-opacity-25");
+	const [colorRojo,setColorRojo]= useState("bg-opacity-25");
 
-	function saludar() {
-		// alert(state)
-		if(state === "Hola"){
-			setState("If you see this green button... bootstrap is working...")
-		}else{
-			setState("Hola")
+	// function saludar() {
+	// 	// alert(state)
+	// 	if(state === "Hola"){
+	// 		setState("If you see this green button... bootstrap is working...")
+	// 	}else{
+	// 		setState("Hola")
+	// 	}
+	// }
+
+	function cambiarColorVerde() {
+		if (colorVerde === "bg-opacity-25") {
+			setColorVerde("bg-opacity-100");
+			setColorRojo("bg-opacity-25");
+			setColorAmarillo("bg-opacity-25");
+		} else {
+			setColorVerde("bg-opacity-25");
+		}
+	 }
+
+	function cambiarColorAmarillo() {
+
+		if (colorAmarillo === "bg-opacity-25") {
+			setColorAmarillo("bg-opacity-100");
+			setColorRojo("bg-opacity-25");
+			setColorVerde("bg-opacity-25");
+		}	else {
+			setColorAmarillo("bg-opacity-25");
 		}
 	}
 
-	function cambiarColor() {
-		// if(color === "btn-success"){//true
-		// 	setColor("btn-danger")
-		// }else{//false
-		// 	setColor("btn-success")
-		// }
-		// condicion ? true : false
-		color === "btn-success"? setColor("btn-danger"):setColor("btn-success")
-		state === "Hola"? setState("If you see this green button... bootstrap is working..."):setState("Hola")
+	function cambiarColorRojo() {
+		if (colorRojo=== "bg-opacity-25") {
+			setColorRojo("bg-opacity-100");
+			setColorVerde("bg-opacity-25");
+			setColorAmarillo("bg-opacity-25");
+		}	else {
+			setColorRojo("bg-opacity-25");
+		}
 	}
-console.log(state);
+
+console.log (colorRojo);
+console.log (colorAmarillo);
+console.log (colorVerde);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			                    {/* btn-success */}
-			<a href="#" className={"btn "+color} onClick={cambiarColor}>
-				{/* "hola" */}
-				{state}
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="card justify-content-center ">
+			<div className="card-body justify-content-center bg-dark" style={{height: '400px', width: '125px', paddingTop:'50px', paddingLeft:'25px'}}>
+				<div style={{height: '80px', width: '75px'}} className= {"bg-danger p-2 text-dark " +colorRojo+ " rounded-circle"} onClick = {cambiarColorRojo}></div>
+				<br></br>
+				<div style={{height: '80px', width: '75px'}} className= {"bg-warning p-2 text-dark " +colorAmarillo+ " rounded-circle"} onClick = {cambiarColorAmarillo}></div>
+				<br></br>
+				<div style={{height: '80px', width: '75px'}} className= {"bg-success p-2 text-dark " +colorVerde+ " rounded-circle"} onClick = {cambiarColorVerde}></div>
+			</div>
 		</div>
 	);
 };
